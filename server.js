cd47e9045ad7ff8e552ae9ff6c62b1023b363d54
Erlend Eriksen <erlend_ne@hotmail.com>
2017-03-17 23:01:54 +0000

    send json responses

--- server.js
+++ server.js
@@ -19,18 +19,46 @@ app.post('/addUser', function(request, response) {
     console.log(request.body);      // print json object
     var username = request.body.username;
     var password = request.body.password;
-    response.send(request.body);
 
     connectDb(function (db) {
         db.collection("users").findOne({"username": username},
             function (err, collection) {
                 if (collection) {
                     console.log("Username is taken")
+                    response.json({"status": "taken"});
                 }
                 else {  
                     db.collection('users').insertOne( {"username": username, "password": password});
                     console.log("User added to database");
                     db.close(); // Only close database if an user has been added (prevents instance pool destroyed error).
+                    response.json({"status": "ok"});
+                }
+            });
+    });
+});
+
+app.get('/login',function(request,response){
+    console.log(request.body);      // print json object
+    var username = request.body.username;
+    var password = request.body.password;
+
+    connectDb(function (db) {
+        db.collection("users").findOne({ "username": username },
+            function (err, collection) {
+                if (collection) {
+                    console.log("user exists");
+                    if (collection.password == password){
+                        console.log("login success");
+                        response.json({"status": "ok"});
+                    }
+                    else{
+                        console.log("wrong password");
+                        response.json({"status": "wrong"});
+                    }
+                }
+                else {
+                    console.log("no user");
+                    response.json({"status": "none"});
                 }
             });
     });
